131943520f42b7a03cd136f1d43b73159a6368f7
sharatthota <64753407+sharatthota@users.noreply.github.com>
2022-03-08 21:07:21 +0100

    updated

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MedOauth.Startup))]
+
+namespace MedOauth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
